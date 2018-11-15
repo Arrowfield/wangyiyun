9e4419c8d645e3cfbffec84fae228e5468455597
Arrowfield <768449566@qq.com>
2018-11-15 10:13:01 +0800

    update

--- pool.js
+++ pool.js
@@ -1,11 +1,31 @@
 /*基于mysql模块构建数据库池*/
 const mysql=require('mysql');
 const pool=mysql.createPool({
-	host:'ltuizoczyjpu.mysql.sae.sina.com.cn',
-	port:'10062',
+	host:'127.0.0.1',
+	port:'3306',
 	user:'root',
 	password:'',
 	database:'wyy',
 	connectionLimit:10
 });
 module.exports=pool;
+//数据库不存在【Handshake inactivity timeout】
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
