80a83db3026f579a44401365b7068f99510b5d94
mcan <768449566@qq.com>
2019-01-22 14:22:01 +0800

    "update"

---
+++ tp/application/index/controller/Login.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+//[控制器 返回数据 C]
+class L extends Controller
+{
+    public function create(){
+        //加载登录模板
+        return view();
+    }
+}
