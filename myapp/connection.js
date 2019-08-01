081814b1100b397d425a8c02cddaf49b2b3927e4
field <768449566@qq.com>
2019-08-01 17:06:21 +0800

    将跟当前项目不相关的文件全部删除

--- myapp/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-var connection = mysql.createConnection({
-  host:"127.0.0.1",
-  user:"root",
-  password:"root",
-  database:"wyy",
-  connectionLimit:10
-});
-module.exports = connection;
