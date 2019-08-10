0817595b09d103b4657b641159282c9196fa379e
Arrowfield <768449566@qq.com>
2019-08-10 15:12:41 +0800

    update

---
+++ application/api/controller/web/Index.php
@@ -0,0 +1,13 @@
+<?php 
+namespace app\api\controller\web;
+
+use think\Controller;
+
+class Index extends Controller{
+
+    publuc function index(){
+        return "456456";
+    }
+}
+
+//http://www.mydemo.com/api/web/index/index
